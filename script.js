797293efacb3607084b10bc321178153f8a7dd81
Tasha <tashadifa@gmail.com>
2020-11-30 21:33:27 +0700

    Updated toggle

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function myFunction() {
-    var x = document.getElementById('navbar');
-    if (x.className === 'topbar') {
-        x.className += ' responsive' ;
+const toggle = document.querySelector('.toggle');
+const topbar = document.querySelector(".topbar");
+
+function toggleTopbar() {
+    if (topbar.classList.contains("active")) {
+        topbar.classList.remove("active");
+
+        toggle.querySelector('a').innerHTML = '<img src="./images/icon-hamburger.svg" />';
     } else {
-        x.className = 'topbar';
+        topbar.classList.add("active");
+
+        toggle.querySelector('a').innerHTML = '<img src="./images/icon-close.svg" />';
     }
-}
+}  
+
+toggle.addEventListener('click', toggleTopbar, false);
