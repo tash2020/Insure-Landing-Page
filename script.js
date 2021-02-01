a1e19f32489494ea8690e32be0ffd1918bc3b95a
Tasha <tashadifa@gmail.com>
2021-02-01 22:16:00 +0700

    Updated overlay and animations.

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const hamburger = document.querySelector('#hamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 hamburger.addEventListener('click', function () {
-  if (hamburger.classList.contains('open')) {
-    hamburger.classList.remove('open');
+  console.log('click hamburger');
+
+  if (header.classList.contains('open')) {
+    header.classList.remove('open');
+    overlay.classList.remove('slide-in');
+    overlay.classList.add('slide-out');
   } else {
-    hamburger.classList.add('open');
+    header.classList.add('open');
+    overlay.classList.remove('slide-out');
+    overlay.classList.add('slide-in');
   }
 });
