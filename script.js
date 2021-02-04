1683343350f3729258822e10afcc64594735ae2a
Tasha <tashadifa@gmail.com>
2021-02-04 21:49:59 +0700

    Updated fixed btn hamburger.

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 let btnHamb = document.querySelector('.header__hamburger');
+let body = document.querySelector('body');
 let header = document.querySelector('.header');
 let overlay = document.querySelector('.overlay');
+let visible = document.querySelectorAll('.visibility');
 
 btnHamb.addEventListener('click', function () {
   if (header.classList.contains('active')) {
+    body.classList.remove('noscroll');
     header.classList.remove('active');
-    overlay.classList.remove('slide-in');
-    overlay.classList.add('slide-out');
+    visible.forEach(function (element) {
+      element.classList.remove('slide-in');
+      element.classList.add('slide-out');
+    });
   } else {
+    body.classList.add('noscroll');
     header.classList.add('active');
-    overlay.classList.remove('slide-out');
-    overlay.classList.add('slide-in');
+    visible.forEach(function (element) {
+      element.classList.remove('slide-out');
+      element.classList.add('slide-in');
+    });
   }
 });
